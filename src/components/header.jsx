e79e5f4455a36d1741177d31e71e13b20fdc53c6
EtoNeAnanasbI95 <ananasovii.yashik@gmail.com>
2024-06-25 10:38:18 +0300

    some fix

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,19 +2,30 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <Navbar expand="lg" bg="primary" data-bs-theme="dark" >
       <Container>
-        <Navbar.Brand href="/Web/">NotPineapple shop</Navbar.Brand>
+        <Navbar.Brand >
+          <Link to="/Web/</Navbar.Brand>">NotPineapple shop</Link>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/Web/">Home</Nav.Link>
-            <Nav.Link href="/Web/Cart/">Сatalog</Nav.Link>
-            <Nav.Link href="/Web/favourites/">Favourites</Nav.Link>
-            <Nav.Link href="/Web/overlay/">Basket</Nav.Link>
+            <Nav.Link>
+              <Link to="/Web/">Home</Link>
+            </Nav.Link>
+            <Nav.Link href="/Web/Cart/">
+              <Link to="/Web/Cart/">Сatalog</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="/Web/favourites/">Favourites</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="/Web/overlay/">Basket</Link>
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
